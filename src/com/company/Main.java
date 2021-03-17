888603066b1c2d42c71c6f1ff939553e4a3f24fb
zaba1997 <zabagamer74@gmail.com>
2021-03-17 18:51:35 +0100

    Add Main.java to repo

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println();
+        System.out.println(":)");
+        System.out.println("SSS");
 	// write your code here
     }
 }
