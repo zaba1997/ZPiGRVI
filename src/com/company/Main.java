5652ea6da8ec5f9ff01ee02edeaf2989f33c4619
zaba1997 <zabagamer74@gmail.com>
2021-04-07 18:43:54 +0200

    Add file Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,12 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println();
-        System.out.println(":)");
-        System.out.println("SSS");
-        System.out.println("ssssssss");
-        System.out.println("ssss");
-        System.out.println("Elowhfdhdfhfda ni");
+        System.out.println("Witam witam witam");
 	// write your code here
     }
 }
